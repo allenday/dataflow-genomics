03c4c9913af91e42a7b08a48feb1e78d6a90bcd4
pseveryn <pseverin@ukr.net>
2019-10-30 16:59:53 +0200

    fix files deletion

---
+++ src/main/java/com/google/allenday/genomics/core/model/BamWithIndexUris.java
@@ -0,0 +1,22 @@
+package com.google.allenday.genomics.core.model;
+
+import java.io.Serializable;
+
+public class BamWithIndexUris implements Serializable {
+
+    private String bamUri;
+    private String indexUri;
+
+    public BamWithIndexUris(String bamUri, String indexUri) {
+        this.bamUri = bamUri;
+        this.indexUri = indexUri;
+    }
+
+    public String getBamUri() {
+        return bamUri;
+    }
+
+    public String getIndexUri() {
+        return indexUri;
+    }
+}

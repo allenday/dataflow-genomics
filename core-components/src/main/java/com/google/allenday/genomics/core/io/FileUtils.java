02d083ee7050430b7aad09be56bce3526f1c6a1b
pseveryn <pseverin@ukr.net>
2019-12-11 16:40:32 +0200

    change hierarchy and add sra_converter

---
+++ core-components/src/main/java/com/google/allenday/genomics/core/io/FileUtils.java
@@ -0,0 +1,117 @@
+package com.google.allenday.genomics.core.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtils implements Serializable {
+
+    private static Logger LOG = LoggerFactory.getLogger(FileUtils.class);
+
+    public void saveDataToFile(byte[] data, String filename) throws IOException {
+        File destFile = new File(filename);
+        destFile.createNewFile();
+        OutputStream os = new FileOutputStream(new File(filename));
+        os.write(data);
+        os.close();
+    }
+
+    public String getCurrentPath(){
+        Path currentRelativePath = Paths.get("");
+        String currentPath = currentRelativePath.toAbsolutePath().toString();
+        if (currentPath.charAt(currentPath.length() - 1) != '/'){
+            currentPath = currentPath + '/';
+        }
+        return currentPath;
+    }
+
+    public String makeDirByCurrentTimestampAndSuffix(String suffix) throws RuntimeException{
+        String workingDir = getCurrentPath() + System.currentTimeMillis() + "_" + suffix + "/";
+        mkdirFromUri(workingDir);
+        return workingDir;
+    }
+
+    public void mkdirFromUri(String path) throws RuntimeException{
+        Path dir;
+        if (path.charAt(path.length() - 1) != '/') {
+            dir = Paths.get(path).getParent();
+        } else {
+            dir = Paths.get(path);
+        }
+        if (!Files.exists(dir)) {
+            try {
+                Files.createDirectories(dir);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            LOG.info(String.format("Dir %s created", dir.toString()));
+        }
+    }
+
+    public byte[] readFileToByteArray(String filePath) throws IOException {
+        return Files.readAllBytes(new File(filePath).toPath());
+    }
+
+    public long getFileSizeMegaBytes(String filePath) {
+        return new File(filePath).length() / (1024 * 1024);
+    }
+
+    public String getFilenameFromPath(String filePath) {
+        if (filePath.charAt(filePath.length() - 1) == '/'){
+            throw new RuntimeException("There is no file in path");
+        }
+        if (filePath.contains("/")) {
+            return filePath.split("/")[filePath.split("/").length - 1];
+        } else {
+            return filePath;
+        }
+    }
+
+    public String changeFileExtension(String fileName, String newFileExtension) {
+        if (fileName.contains(".")) {
+            return fileName.split("\\.")[0] + newFileExtension;
+        } else {
+            return fileName + newFileExtension;
+        }
+    }
+
+
+    public void deleteFile(String filePath) {
+        File fileToDelete = new File(filePath);
+        if (fileToDelete.exists()) {
+            boolean delete = fileToDelete.delete();
+            if (delete) {
+                LOG.info(String.format("File %s deleted", filePath));
+            }
+        }
+    }
+
+    public void deleteDir(String dirPath) {
+        try {
+            org.apache.commons.io.FileUtils.deleteDirectory(new File(dirPath));
+            LOG.info(String.format("Dir %s deleted", dirPath));
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
+    }
+
+    public long getFreeDiskSpace() {
+        return new File("/").getFreeSpace();
+    }
+
+    public boolean exists(String filePath) {
+        return Files.exists(Paths.get(filePath));
+    }
+
+    public boolean contentEquals(File file1, File file2) throws IOException{
+        return org.apache.commons.io.FileUtils.contentEquals(file1, file2);
+    }
+
+    public InputStream getInputStreamFromFile(String filePath) throws FileNotFoundException {
+        return new FileInputStream(filePath);
+    }
+}
---
+++ core-components/src/main/java/com/google/allenday/genomics/core/io/UriProvider.java
@@ -0,0 +1,29 @@
+package com.google.allenday.genomics.core.io;
+
+import com.google.allenday.genomics.core.model.GeneExampleMetaData;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UriProvider implements Serializable {
+
+    private String srcBucket;
+    private ProviderRule providerRule;
+
+    public UriProvider(String srcBucket, ProviderRule providerRule) {
+        this.srcBucket = srcBucket;
+        this.providerRule = providerRule;
+    }
+
+    public void setProviderRule(ProviderRule providerRule) {
+        this.providerRule = providerRule;
+    }
+
+    public List<String> provide(GeneExampleMetaData geneExampleMetaData) {
+        return providerRule.provideAccordinglyRule(geneExampleMetaData, srcBucket);
+    }
+
+    public interface ProviderRule extends Serializable{
+        List<String> provideAccordinglyRule(GeneExampleMetaData geneExampleMetaData, String srcBucket);
+    }
+}
